37d6c0bd65fd44dc2b5fba314f3920c29f3dadd2
Agandaur <cjwagstaffe@gmail.com>
2020-07-27 16:27:07 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { auth } from './services/firebase';
+
+function PrivateRoute({ component: Component, authenticated, ...rest}) {
+  return (
+    <Route 
+      {...rest}
+      render={(props) => authenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state : { from: props.location} }} />}
+    />
+  )
+}
+
+function PublicRoute({ component: Component, authenticated, ...rest}) {
+  return (
+    <Route 
+      {...rest}
+      render={(props) => authenticated === false
+      ? <Component {...props} />
+      : <Redirect to='/chat' />}
+    />
+  )
+}
 
 function App() {
   return (
